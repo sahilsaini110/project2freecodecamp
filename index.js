4507be1074c563a4259c74cdf9097c5219cc3e51
Sahil <sohil170246@gmail.com>
2022-06-26 12:37:05 +0530

    first commit

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    "ipaddress": 'hello API',
+    "language": 'hello API',
+    "software": 'hello API'
+});
 });
 
 
